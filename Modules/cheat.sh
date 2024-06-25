020beed0f00056073a6b91215bbc84ca8b0809cd
Lua van Loon <git@luatess.email>
2024-06-25 12:03:12 +0200

    added Cheat module

---
+++ Modules/cheat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+PREREQUISITES=('wget')
+PACKAGES=()
+
+installPackages PREREQUISITES[@] PACKAGES[@]
+
+currDir=$(echo "$PWD")
+
+cd /tmp \
+    && wget https://github.com/cheat/cheat/releases/download/4.4.2/cheat-linux-amd64.gz \
+    && gunzip cheat-linux-amd64.gz \
+    && chmod +x cheat-linux-amd64 \
+    && sudo mv cheat-linux-amd64 /usr/local/bin/cheat
+
+cd $currDir
+
+yes | cheat
