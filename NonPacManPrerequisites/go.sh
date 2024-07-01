e347757d56879452cde3ad1c664b6dde2f0236ba
Lua van Loon <git@luatess.email>
2024-07-02 00:11:38 +0200

    bugfix: fixed go download via curl - redirect appeared

--- NonPacManPrerequisites/go.sh
+++ NonPacManPrerequisites/go.sh
@@ -4,7 +4,7 @@ if ! command -v go &> /dev/null; then
     profile=$(eval echo "~$USER")/.profile
     curlFileName="$(curl https://go.dev/VERSION?m=text | head -n 1).linux-amd64.tar.gz"
 
-    curl "https://go.dev/dl/$curlFileName" -O
+    curl -OL "https://go.dev/dl/$curlFileName"
     rm -rf /usr/local/go && tar -C /usr/local -xzf "./$curlFileName"
     echo 'export PATH=$PATH:/usr/local/go/bin' >> $profile
 
