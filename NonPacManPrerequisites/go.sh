c46b85a5a8baaea2284d858c0514649b386ab27c
Lua van Loon <git@luatess.email>
2024-07-01 23:44:32 +0200

    bugfix: fixed changing of filetype with curl -o flag

--- NonPacManPrerequisites/go.sh
+++ NonPacManPrerequisites/go.sh
@@ -2,10 +2,12 @@ echo "Installing go"
 
 if ! command -v go &> /dev/null; then
     profile=$(eval echo "~$USER")/.profile
+    curlFileName="$(curl https://go.dev/VERSION?m=text | head -n 1).linux-amd64.tar.gz"
 
-    curl "https://go.dev/dl/$(curl https://go.dev/VERSION?m=text | head -n 1).linux-amd64.tar.gz" -o ./go.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf ./go.tar.gz
+    curl "https://go.dev/dl/$curlFileName" -O
+    rm -rf /usr/local/go && tar -C /usr/local -xzf "./$curlFileName"
     echo 'export PATH=$PATH:/usr/local/go/bin' >> $profile
+
     # @TODO verify installation
     echo "go has been installed"
 else
