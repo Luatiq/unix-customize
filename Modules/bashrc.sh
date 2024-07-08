85a8270b2f90537f48a910b2b8379ac8265abd64
Lua van Loon <me@luatess.email>
2024-07-08 23:52:43 +0200

    changed bashrc to make ~/Documents/Projects directory if it doesn't exist already so that alias www doesn't fail

--- Modules/bashrc.sh
+++ Modules/bashrc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+mkdir -p ~/Documents/Projects
+
 echo "alias www='cd ~/Documents/Projects/'" >> ./.profile
 echo "alias vrc='cd ~/.config/nvim/ && nvim .'" >> ./.profile
 echo "export VISUAL=nvim" >> ./.profile
